ad7bc6ddd7588fa5e3dc10efac79820af37897af
Gunasekar Jabbala <gunasekar.ai.dev@gmail.com>
2022-10-01 20:57:54 +0530

    hr_ret_data_preprocessing

---
+++ HR_Retention_analysis/main.py
@@ -0,0 +1,11 @@
+
+__Author__ = "Gunasekar Jabbala"
+__Email__ = "gunasekar.ai.dev@gmail.com"
+
+# Retention Study - Figuring out which Employees may quit
+
+# OSEMN Process - stands for Obtain, Scrub, Explore, Model and Interpret.
+
+# Import Libraries
+
+
